9ab05b90546cda285797a6dae1520bf81f80a85b
manu <you@example.com>
2023-10-23 16:41:15 +0200

    V0.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Que pasa Paco");
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Introduce un valor");
+        int a = entrada.nextInt();
+        System.out.println("introduce otro valor");
+        int b = entrada.nextInt();
+        if (a<b){
+            System.out.println(b+" Es el mayor");
+        }else if(a>b){
+            System.out.println(a+" Es el mayor");
+        }else{
+            System.out.println("error");
+        }
+
     }
 }
